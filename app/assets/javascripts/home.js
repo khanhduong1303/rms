8dd26caa3225095a19e045d6ec3dd32bae3fddf7
Hung Nguyen <khanh1111527@gmail.com>
2014-07-09 11:14:00 +0700

    change home.js

--- app/assets/javascripts/home.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kt on 01/07/2014.
- */
-$(document).ready(function(){
-    $('#emailIcon').click(function(e){
-        e.preventDefault();
-        $('#emailSelect').select();
-    });
-});
