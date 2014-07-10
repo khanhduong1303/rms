4984518e63ae6c65bb9e5e83455ddda066bab555
Hung Nguyen <khanh1111527@gmail.com>
2014-07-10 18:55:44 +0700

    Change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -4,14 +4,14 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to save the application database on another
+# database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707081930) do
+ActiveRecord::Schema.define(version: 20140708101913) do
 
   create_table "bulletins", force: true do |t|
     t.string   "title",                      null: false
