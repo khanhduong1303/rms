780b9897bd1e9e6881541a3e42790f5008faa072
Shinnosuke <thienthankhoxx@gmail.com>
2014-07-18 11:20:40 +0700

    abc

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,17 @@
 class Api::ApiController < ActionController::Base
   respond_to :json
+  before_filter :authenticate_user_from_token!
+
+
+  def authenticate_user_from_token!
+user = User.find_by_authentication_token(params[:authentication_token])
+unless user 
+    render json: {
+    status: "false",
+    message: "You need sign in/sign up to continue",
+    result:{}
+
+} , status: 401
+end
 end
 
