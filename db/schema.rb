0329480d0e0a8f105aac541133d2196ca9652cf1
kt <khanh1111527@gmail.com>
2014-07-22 10:01:13 +0700

    complete api check booking

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20140721170012) do
     t.date     "date",                       null: false
     t.text     "content",                    null: false
     t.boolean  "send_notify", default: true
-    t.integer  "condo_id",                   null: false
+    t.integer  "user_id",                    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -75,24 +75,24 @@ ActiveRecord::Schema.define(version: 20140721170012) do
     t.string   "location"
     t.string   "organiser"
     t.text     "description"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "condo_id"
   end
 
   create_table "facilities", force: true do |t|
-    t.string   "name",                                   null: false
-    t.float    "booking_price",                          null: false
-    t.float    "deposit_price",                          null: false
+    t.string   "name"
+    t.integer  "facility_category_id"
+    t.float    "booking_price"
+    t.float    "deposit_price"
     t.text     "note"
-    t.string   "image_path_file_name"
-    t.string   "image_path_content_type"
-    t.integer  "image_path_file_size"
-    t.datetime "image_path_updated_at"
-    t.boolean  "active",                  default: true
-    t.integer  "facility_category_id",                   null: false
+    t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "facility_categories", force: true do |t|
@@ -103,14 +103,10 @@ ActiveRecord::Schema.define(version: 20140721170012) do
   end
 
   create_table "facility_statuses", force: true do |t|
-    t.string   "image_path_file_name"
-    t.string   "image_path_content_type"
-    t.integer  "image_path_file_size"
-    t.datetime "image_path_updated_at"
+    t.integer  "facility_id"
     t.date     "close_from"
     t.date     "close_to"
     t.text     "reason"
-    t.integer  "facility_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
