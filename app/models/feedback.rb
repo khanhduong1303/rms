98b8bd80472dccdb08d1944ed7ae71253cdc89ef
Shinnosuke <thienthankhoxx@gmail.com>
2014-07-25 16:41:23 +0700

    fix api

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,6 @@ class Feedback < ActiveRecord::Base
 	has_one :feedback_reply ,dependent: :destroy
 	belongs_to :feedback_category
 	belongs_to :user
+	validates :user_id, presence: true
+	validates :feedback_category_id, presence: true
 end
