ecd164ff151bd4e5905eb5159e8c5cd8afe32daf
kt <khanh1111527@gmail.com>
2014-08-14 21:39:56 +0700

    change courses api

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -4,40 +4,84 @@ class Api::CoursesController < Api::ApiController
   skip_before_action :authenticate_user_from_token!
 
   def index
-    unless @courses.nil?
-      render json: {status: 'success', message: 'Found courses', total: @courses.count, data: @courses}, status: :ok
-    else
-      render json: {status: 'failed', message: 'Not found courses', data: {}}, status: :not_found
+    limit = params[:limit].to_i
+    page = params[:page].to_i
+    if params[:condo_id].nil?
+      return render json: PublicFunction.data_json('failed', 'Missing user_id parameter', 0, nil)
+    end
+    if page < 1 or limit < 1
+      @results = Course.limit(10).where(condo_id:params[:condo_id])
+      @courses = process_results @results, []
+      return render json: PublicFunction.data_json('success', 'Courses list', @courses.size, @courses)
+    end
+
+    if limit > 0 and page > 0
+      @results = Courses.limit(limit).offset(page*limit-limit).where(condo_id:params[:condo_id])
+      @courses = process_results @results, []
+      if @courses.size > 0
+        return render json: PublicFunction.data_json('success', 'Courses list', @courses.size, @courses)
+      else
+        @results = Courses.limit(10).where(condo_id:params[:condo_id])
+        @courses = process_results @results, []
+        return render json: PublicFunction.data_json('success', 'Courses list', @courses.size, @courses)
+      end
     end
   end
 
   def show
-    unless @course.nil?
-      render json: {status: 'success', message: 'Found course', data: @course}, status: :ok
+    course_id  = params[:course_id ]
+    if Course.where(:id => course_id ).size > 0
+      render json: PublicFunction.data_json('success', 'Show Course', 1, process_results(Course.where(id:course_id ), {}) )
     else
-      render json: {status: 'failed', message: 'Not found course', data: {}}, status: :not_found
+      render json: PublicFunction.data_json('failed', 'Course not found', 0, nil)
     end
   end
-
+  
+  
   private
-    def set_course
-      begin
-        @course = Course.select(:id, :name, :image_path_file_name, :description, :phone, :email, :more_info).find(params[:course_id])
-      rescue ActiveRecord::RecordNotFound => e
-        @course = nil
+  def process_results results=nil, type=[]
+    if type.is_a?(Array)
+      courses_data=type
+      i=0
+      results.each do |course|
+        temp = Hash.new
+        temp[:id]= course.id
+        temp[:name]=course.name
+        temp[:description]=course.description
+        temp[:instructor]=course.instructor
+        temp[:price]=course.price
+        temp[:phone]=course.phone
+        temp[:email]=course.email
+        temp[:image_path]=course.image_path.url
+        temp[:more_info]=course.more_info
+        temp[:condo_id]=course.condo_id
+        temp[:created_at]=course.created_at
+        temp[:updated_at]=course.updated_at
+        courses_data[i] = temp
+        #temp[b.status] = b.status
+        #facilities[i][:ok] = 'ok'
+        i+=1
+      end
+      return courses_data
+    else
+      results.each do |course|
+        temp = Hash.new
+        temp[:id]= course.id
+        temp[:name]=course.name
+        temp[:description]=course.description
+        temp[:instructor]=course.instructor
+        temp[:price]=course.price
+        temp[:phone]=course.phone
+        temp[:email]=course.email
+        temp[:image_path]=course.image_path.url
+        temp[:more_info]=course.more_info
+        temp[:condo_id]=course.condo_id
+        temp[:created_at]=course.created_at
+        temp[:updated_at]=course.updated_at
+        return temp
       end
     end
-
-    def set_courses
-      begin
-        @limit = params[:limit]
-        @page = params[:page]
-        @limit = @limit.nil? ? 5 : @limit.to_i
-        @page = @page.nil? ? 1 : @page.to_i
-        @courses = Course.select(:name, :instructor, :price).where(condo_id: params[:condo_id]).limit(@limit).offset((@page - 1) * @limit)
-      rescue Exception => e
-        @courses = nil
-      end
-    end
+  end
+  
 end
 
