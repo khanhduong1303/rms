4fa1fd2cfaaa8108197401fbcee751509dfb8a38
Hung Nguyen <khanh1111527@gmail.com>
2014-08-15 16:18:44 +0700

    remove user params when pass params api

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,12 +38,7 @@ class User < ActiveRecord::Base
                        :size => {:in => 0..3.megabytes},
                        :content_type => {:content_type => /^image\/(jpeg|png|gif|tiff)$/}
   validates :email, uniqueness: true
-#  validates :username, presence: true
   validates :phone, presence: true
- # validates :postal_code, presence: true
- # validates :enquiry, presence: true
-  validates :username, length: {minimum: 4, maximun: 14}
-  validates :username, uniqueness: true
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
