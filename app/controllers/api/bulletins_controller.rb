0b7c765fd2907369ba645d5a96f32aa36601cd88
Hung Nguyen <khanh1111527@gmail.com>
2014-08-22 14:25:23 +0700

    add column to bulletins

--- app/controllers/api/bulletins_controller.rb
+++ app/controllers/api/bulletins_controller.rb
@@ -4,7 +4,7 @@ class Api::BulletinsController < Api::ApiController
 
   def index
     unless @bulletins.nil?
-      render json: {status: 'success', message: 'Found bulletins', total: @total, results: @bulletins}, status: :ok
+      render json: {status: 'success', message: 'Found bulletins', total: @total, results: @bulletins.to_json(only: [:id])}, status: :ok
     else
       render json: {status: 'failed', message: 'Not found bulletins', results: {}}, status: :not_found
     end
@@ -21,7 +21,7 @@ class Api::BulletinsController < Api::ApiController
   private
     def set_bulletin
       begin
-        @bulletin = Bulletin.select(:id, :title, :date, :content).find(params[:bulletin_id])
+        @bulletin = Bulletin.select(:id, :title, :date, :content, :image_path_file_name).find(params[:bulletin_id])
       rescue ActiveRecord::RecordNotFound => e
         @bulletin = nil
       end
@@ -36,7 +36,7 @@ class Api::BulletinsController < Api::ApiController
         @page = @page.nil? ? 1 : @page.to_i
         condo = User.find_by_authentication_token(token).condo
         @total = condo.bulletins.all.count
-        @bulletins = condo.bulletins.select(:id, :title, :date ,:content).where(send_notify: true).limit(@limit).offset((@page - 1) * @limit).order(date: :desc)
+        @bulletins = condo.bulletins.select(:id, :title, :date ,:content, :image_path_file_name).where(send_notify: true).limit(@limit).offset((@page - 1) * @limit).order(date: :desc)
       rescue Exception => e
         @bulletins = nil
       end
