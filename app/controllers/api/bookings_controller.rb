7de073bf1b15fb397f4ebfdf8716080d9d58e149
kt <khanh1111527@gmail.com>
2014-08-26 09:49:55 +0700

    fix booking api

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -32,8 +32,8 @@ class Api::BookingsController < Api::ApiController
 
   def make_a_booking
     if !params[:user_id].nil? && !params[:preferred_date].nil? && !params[:time_slot_id].nil?
-      if Booking.where(user_id:params[:user_id], time_slot_id:params[:time_slot_id]).size > 0
-        return render json: PublicFunction.data_json('failed', 'Can\'t booking again', 0, nil)
+      if Booking.where(date_book:params[:preferred_date].to_date, time_slot_id:params[:time_slot_id]).size > 0
+        return render json: PublicFunction.data_json('failed', 'Booking existed!', 0, nil)
       end
       @booking = Booking.create(time_slot_id:params[:time_slot_id], date_submit:Time.now , date_expiry:1.days.from_now , date_book:params[:preferred_date], user_id:params[:user_id], status:'Reserved')
       if @booking
